697f1fc67ed06642182ccb89b04014393642c243
Gustavo Oliveira <gustavaohenrique@hotmail.com>
2024-01-30 20:56:44 -0300

    Version 0.0.1

---
+++ src/dtos/inputs/create-appointment-input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class CreateAppointmentInput {
+	@Field()
+	customerId: string;
+	@Field()
+	startsAt: Date;
+	@Field()
+	endsAt: Date;
+}
